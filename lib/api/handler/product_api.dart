5132b34bd969984c7f7f030f18d4d8fd78416a0c
Nico Enriquez <awesometoast357@gmail.com>
2023-10-28 02:52:02 +0800

    feat: modified product_api.dart

--- lib/api/handler/product_api.dart
+++ lib/api/handler/product_api.dart
@@ -1,4 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:products_practice/api/api_client.dart';
+import 'package:products_practice/model/product_model.dart';
+
+//Sample api
+//https://dummyjson.com/products
+// "id":1,
+// "title":"iPhone 9",
+// "description":"An apple mobile which is nothing like apple",
+// "price":549,
+// "discountPercentage":12.96,
+// "rating":4.69,
+// "stock":94,
+// "brand":"Apple",
+// "category":"smartphones",
+// "thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+// "images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg",
+// "https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]}
 
 class ProductApi {
   ProductApi({
@@ -8,4 +25,19 @@ class ProductApi {
 
   final ApiClient apiClient;
   final Uri baseUrl;
+
+  Future<ProductMethod> getProductList({required String offset, required String limit}) async {
+    final queryParam = <String, dynamic>{};
+
+    queryParam['limit'] = limit;
+    queryParam['offset'] = offset;
+
+    final uri = baseUrl.replace(
+      queryParameters: queryParam,
+      path: '${baseUrl.path}',
+    );
+
+    Response response = await apiClient.dio.getUri(uri);
+    return ProductMethod.fromJson(response.data);
+  }
 }
--- lib/screens/README.txt
+++
@@ -1,1 +0,0 @@
-this is for the screens will be removed soon along with the other READMES
